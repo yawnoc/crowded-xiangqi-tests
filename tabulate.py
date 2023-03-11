76809cff1a4fcab9fafac88287fa69fd9b7a64a5
Conway <yawnoc@pm.me>
2023-03-11 14:16:13 +0800

    Create tabulate.py

---
+++ tabulate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+# tabulate.py
+
+Tabulate the win statistics in `*/*/*.pgn`, updating `README.md`.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
